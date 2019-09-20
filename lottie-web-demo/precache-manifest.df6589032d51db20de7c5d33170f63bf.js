482ce06cb218e115191e5e89c58fc4f1bbb6805c
Joseph Khan <joseph.khan@altayyargroup.com>
2019-09-21 01:16:31 +0400

    Lottie Web Demo committed
    Lottie animation integrated to a React App. The react app is built using create-react-app.

---
+++ lottie-web-demo/precache-manifest.df6589032d51db20de7c5d33170f63bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5097ab429868f6237daa1566a5fb73e3",
+    "url": "/lottie-web-demo/index.html"
+  },
+  {
+    "revision": "36db1aa314ad4166a2c3",
+    "url": "/lottie-web-demo/static/css/main.78ca29b9.chunk.css"
+  },
+  {
+    "revision": "93e7e1c1b6ba0b323b49",
+    "url": "/lottie-web-demo/static/js/2.466a4e8e.chunk.js"
+  },
+  {
+    "revision": "36db1aa314ad4166a2c3",
+    "url": "/lottie-web-demo/static/js/main.a013f77c.chunk.js"
+  },
+  {
+    "revision": "7c757d247069a7bad267",
+    "url": "/lottie-web-demo/static/js/runtime-main.9764c17f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/lottie-web-demo/static/media/logo.25bf045c.svg"
+  }
+]);
